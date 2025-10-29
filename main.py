085863ef917716d3b2d525537eda3635974fbe88
tomlloydDEV <tom11b@hotmail.co.uk>
2025-10-29 21:09:40 +0000

    fix issue with char dictionary

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ def get_book_text(filepath):
 def main():
   book_text = get_book_text("books/frankenstein.txt")
   num_words = word_count(book_text)
-  num_chars = char_count(book_text)
+  chars = char_count(book_text)
   print(f"Found {num_words} total words")
-  print(num_chars)
+  print(chars)
 
 main()
