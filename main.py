bef272c4478294a57ecfe2b14b15a0ccedcbe244
tomlloydDEV <tom11b@hotmail.co.uk>
2025-10-29 17:24:10 +0000

    remove wordcount function from main

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ def get_book_text(filepath):
   with open(filepath) as f:
     return f.read()
 
-def word_count(book_text):
-  num_words = len(book_text.split())
-  return num_words
 
 def main():
   book_text = get_book_text("books/frankenstein.txt")
