28fee16f4747bf5022f1ffd3ae7cb6bd24653d8b
tomlloydDEV <tom11b@hotmail.co.uk>
2025-10-29 20:56:33 +0000

    add character dictionary

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import word_count
+from stats import char_count
 
 def get_book_text(filepath):
   with open(filepath) as f:
@@ -8,6 +9,7 @@ def get_book_text(filepath):
 def main():
   book_text = get_book_text("books/frankenstein.txt")
   num_words = word_count(book_text)
+  num_chars = char_count(book_text)
   print(f"Found {num_words} total words")
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,15 @@
 def word_count(book_text):
   num_words = len(book_text.split())
   return num_words
+
+def char_count(book_text):
+  book_text = len(book_text.lower())
+  chars = {}
+
+  for c in book_text:
+    if c in chars:
+      chars[c] += 1
+    else:
+      chars[c] = 1
+
+  return chars
